76f5ec6f190b6d44d461e82f606dac5996d61f46
Jan Moringen <scymtym@gmx.net>
2010-10-10 03:48:05 +0200

    Added duplicate-policy to add-to-hook in src/hook.lisp * src/hook.lisp (add-to-hook): added key parameter :duplicate-policy   (add-to-hook t function): added key parameter :duplicate-policy;   handle values :do-nothing, :add, :replace and :error

--- src/hook.lisp
+++ src/hook.lisp
@@ -31,7 +31,7 @@
   (:documentation
    "Replace list of handlers attached to HOOK with NEW-VALUE."))
 
-(defgeneric add-to-hook (hook handler)
+(defgeneric add-to-hook (hook handler &key duplicate-policy)
   (:documentation
    "Add HANDLER to HOOK."))
 
@@ -47,8 +47,40 @@
 ;;; Generic Implementation
 ;;
 
-(defmethod add-to-hook ((hook t) (handler function))
-  (push handler (hook-handlers hook)))
+(defmethod add-to-hook ((hook t) (handler function)
+			&key (duplicate-policy :replace))
+  (bind (((:flet add-it (handler present?))
+	  (values (push handler (hook-handlers hook)) present?))
+	 (present? (when (member handler (hook-handlers hook)) t)))
+    (ecase duplicate-policy
+      ;; If HANDLER is already present, do nothing.
+      (:do-nothing
+       (if present?
+	   (values (hook-handlers hook) t)
+	   (add-it handler nil)))
+
+      ;; Add HANDLER, regardless of whether it is already present or
+      ;; not.
+      (:add
+       (add-it handler present?))
+
+      ;; If HANDLER is already present, replace it. This has the
+      ;; effect of moving HANDLER into the position of the most
+      ;; recently added handler.
+      (:replace
+       (when present?
+	 ;; Do not use remove-from-hook to avoid running possibly
+	 ;; attached behavior.
+	 (removef (hook-handlers hook) handler))
+       (add-it handler present?))
+
+      ;; When adding the same handler twice is not allowed and HANDLER
+      ;; is already present, signal an error.
+      (:error
+       (when present?
+	 (error "Handler ~S already present and policy forbids adding a hook twice"
+		handler))
+       (add-it handler nil)))))
 
 (defmethod remove-from-hook ((hook t) (handler function))
   (removef (hook-handlers hook) handler))
