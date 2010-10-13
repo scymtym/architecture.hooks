61fdded0d53a9f4c5eb904e07e481723633dfaa1
Jan Moringen <scymtym@gmx.net>
2010-10-14 00:35:58 +0200

    Added tests for the hooks and bind integration in test/bind.lisp * test/bind.lisp: new file; unit tests for the hooks and bind   integration

---
+++ test/bind.lisp
@@ -0,0 +1,60 @@
+;;; bind.lisp ---
+;;
+;; Copyright (C) 2010 Jan Moringen
+;;
+;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+;;
+;; This Program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This Program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses>.
+
+(in-package :hooks.test)
+
+(deftestsuite bind (object-hook-test)
+  ()
+  (:documentation
+   "Unit tests for hooks and bind integration"))
+
+(addtest (bind
+	  :documentation
+	  "Smoke test for hooks and bind integration.")
+  smoke
+
+  (bind (((:handler (external-hook object 'my-hook) ())
+	  1))
+    (ensure-same (run-hook (external-hook object 'my-hook)) 1
+		 :test #'=))
+
+  (bind (((:handler (external-hook object 'my-hook) (x))
+	  x))
+    (ensure-same (run-hook (external-hook object 'my-hook) 1) 1
+		 :test #'=))
+
+  (bind (((:handler (external-hook object 'my-hook) ())
+	  1)
+	 ((:handler (external-hook object 'my-hook) ())
+	  2))
+    (ensure-same (run-hook (external-hook object 'my-hook)) 1
+		 :test #'=
+		 :report "Did not see the result of the least recently
+added handler (~S)"
+		 :arguments (1)))
+
+  (bind (((:handler (external-hook object 'my-hook) (x))
+	  x)
+	 ((:handler (external-hook object 'my-hook) (x))
+	  (- x)))
+    (ensure-same (run-hook (external-hook object 'my-hook) 1) 1
+		 :test #'=
+		 :report "Did not see the result of the least recently
+added handler (~S)"
+		 :arguments (1))))
